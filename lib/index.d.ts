f6f79dc790737bd06ba14b988c19d95fc2c975ce
Ran Cohen <rannn505@outlook.com>
2019-07-21 19:07:02 +0300

    fix ts

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+declare module "chrome-dompath" {
+    function _xPathIndex(node: Element): Number;
+}
+export declare const getXPathSelector: (element: Element) => string;
+export declare const getClassChainSelector: (element: Element) => string;
+export declare const parseDomFromString: (xmlString: string) => Document;
