672601b4aa2a1237c8bd2d33cadd26184b159df6
Ran Cohen <rannn505@outlook.com>
2019-07-25 11:16:34 +0300

    fix class chain

--- test/index.test.js
+++ test/index.test.js
@@ -13,37 +13,36 @@ describe('Appium Selector Builder', () => {
   describe('parseDomFromString', () => {
     it('should return an Element', () => expect(parseDomFromString('<div></div>')).to.be.an.instanceOf(Document));
     it('should support tags with $', () => expect(parseDomFromString('<div class="yay.$.yay"></div>').documentElement.outerHTML).to.equal('<div class="yay.$.yay"/>'));
-});
-
-  it('element without attributes', () => {
-    const dom = parseDomFromString('<root><a><b>T1</b><c>T2</c></a></root>');
-    const targetElement = dom.querySelector('b');
-    const selector = getClassChainSelector(targetElement);
-    expect(selector).to.eql('/root/a/b');
   });
 
-  it('element with attributes', () => {
-    const dom = parseDomFromString('<root><a><b name="ran" label="cohen" value="505">T1</b><c>T2</c></a></root>');
-    const targetElement = dom.querySelector('b');
-    const selector = getClassChainSelector(targetElement);
-    expect(selector).to.eql('/root/a/b[`value CONTAINS "505" AND name CONTAINS "ran" AND label CONTAINS "cohen"`]');
-  });
-  
-  it('element with attributes on chain', () => {
-    const dom = parseDomFromString('<root><a name="ran"><b label="cohen" value="505">T1</b><c>T2</c></a></root>');
-    const targetElement = dom.querySelector('b');
-    const selector = getClassChainSelector(targetElement);
-    expect(selector).to.eql('/root/a[`name CONTAINS "ran"`]/b[`value CONTAINS "505" AND label CONTAINS "cohen"`]');
-  });
-
-  it('element with attributes and indexes on chain', () => {
-    const dom = parseDomFromString('<root><a name="ran"><b label="cohen">T1</b><b clickable="true" value="505">T2</b></a></root>');
-    const targetElement = dom.querySelector('b[value="505"]');
-    const selector = getClassChainSelector(targetElement);
-    expect(selector).to.eql('/root/a[`name CONTAINS "ran"`]/b[`value CONTAINS "505"`][2]');
-    expect(getElementAttributesMap(targetElement)).to.eql({
-      value: "505",
-      clickable: true
-    })
+  describe('getClassChainSelector', () => {
+    it('element without attributes', () => {
+      const dom = parseDomFromString('<root><a><b>T1</b><c>T2</c></a></root>');
+      const targetElement = dom.querySelector('b');
+      const selector = getClassChainSelector(targetElement);
+      expect(selector).to.eql('a/b');
+    });
+    it('element with attributes', () => {
+      const dom = parseDomFromString('<root><a><b name="ran" label="cohen" value="505">T1</b><c>T2</c></a></root>');
+      const targetElement = dom.querySelector('b');
+      const selector = getClassChainSelector(targetElement);
+      expect(selector).to.eql('a/b[`value CONTAINS "505" AND name CONTAINS "ran" AND label CONTAINS "cohen"`]');
+    });
+    it('element with attributes on chain', () => {
+      const dom = parseDomFromString('<root><a name="ran"><b label="cohen" value="505">T1</b><c>T2</c></a></root>');
+      const targetElement = dom.querySelector('b');
+      const selector = getClassChainSelector(targetElement);
+      expect(selector).to.eql('a[`name CONTAINS "ran"`]/b[`value CONTAINS "505" AND label CONTAINS "cohen"`]');
+    });
+    it('element with attributes and indexes on chain', () => {
+      const dom = parseDomFromString('<root><a name="ran"><b label="cohen">T1</b><b clickable="true" value="505">T2</b></a></root>');
+      const targetElement = dom.querySelector('b[value="505"]');
+      const selector = getClassChainSelector(targetElement);
+      expect(selector).to.eql('a[`name CONTAINS "ran"`]/b[`value CONTAINS "505"`]');
+      expect(getElementAttributesMap(targetElement)).to.eql({
+        value: "505",
+        clickable: true
+      })
+    });
   });
 });
